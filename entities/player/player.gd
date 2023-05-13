df877e9d134d63e5b527c777bcddf3a0b79bd446
ZeeHatcher <zee.hatcher010@gmail.com>
2023-05-13 20:32:15 +0800

    feat: simple movement animation

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -11,17 +11,20 @@ const INPUTS = {
 }
 
 onready var _ray := $RayCast2D
+onready var _tween := $Tween
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	for action in INPUTS.keys():
 		if event.is_action_pressed(action):
 			var dir: Vector2 = INPUTS[action]
-			if not _will_collide(dir):
+			if not _tween.is_active() and not _will_collide(dir):
 				move(dir)
 
 
 func move(dir: Vector2) -> void:
-	position += GridTranslator.step_isometric(dir)
+	var move_dir := GridTranslator.step_isometric(dir)
+	_animate_movement(move_dir)
 	emit_signal("moved")
 
 
@@ -29,3 +32,10 @@ func _will_collide(dir: Vector2) -> bool:
 	_ray.cast_to = GridTranslator.step_isometric(dir)
 	_ray.force_raycast_update()
 	return _ray.is_colliding()
+
+
+func _animate_movement(dir: Vector2) -> void:
+	_tween.interpolate_property(self, "position",
+		position, position + dir,
+		0.1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	_tween.start()
