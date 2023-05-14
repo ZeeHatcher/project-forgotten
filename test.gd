ad2ba8d9f0a730bc9ada3526c9b85f0e7a25dfc9
ZeeHatcher <zee.hatcher010@gmail.com>
2023-05-14 15:12:20 +0800

    refactor: rename events_repository

--- test.gd
+++ test.gd
@@ -6,7 +6,7 @@ onready var _board := $CanvasLayer/CenterContainer/EventBoard
 
 func _on_EventTiles_event_triggered(event_name: String):
 	get_tree().paused = true
-	_board.event = EventsRepository.get_event(event_name)
+	_board.event = EventRepository.get_event(event_name)
 	_board.show()
 
 
