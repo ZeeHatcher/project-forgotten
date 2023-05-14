5019b4985eedca8b4e5b48f98887520b4c8e37e0
ZeeHatcher <zee.hatcher010@gmail.com>
2023-05-14 17:59:39 +0800

    refactor: use transform2D for isometric projection

--- singletons/grid_translator.gd
+++ singletons/grid_translator.gd
@@ -1,14 +1,16 @@
 extends Node
 
 
+const ISOMETRIC_PROJECTION_MATRIX := Transform2D(Vector2(1.0, 0.5), Vector2(-1.0, 0.5), Vector2.ZERO)
+
 var tile_width := Vector2()
 var tile_height := Vector2()
 var step_distance := 12.0
 
 
 func to_isometric(vector: Vector2) -> Vector2:
-	return Vector2(vector.x - vector.y, (vector.x + vector.y) / 2)
+	return ISOMETRIC_PROJECTION_MATRIX.basis_xform(vector)
 
 
-func step_isometric(vector2: Vector2) -> Vector2:
-	return to_isometric(vector2) * step_distance
+func step_isometric(vector: Vector2) -> Vector2:
+	return to_isometric(vector) * step_distance
