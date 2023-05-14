d9329476c9e5dd3173aeafe486e648ecaa6ee7b0
ZeeHatcher <zee.hatcher010@gmail.com>
2023-05-14 16:28:41 +0800

    feat: event flag conditions

--- misc/event_service.gd
+++ misc/event_service.gd
@@ -5,6 +5,8 @@ extends Node
 export(NodePath) var player
 export(NodePath) var board
 
+var _flags: Dictionary
+
 onready var _player: Player = get_node(player)
 onready var _board: EventBoard = get_node(board)
 
@@ -26,6 +28,34 @@ func end_event():
 	get_tree().paused = false
 
 
-func are_conditions_met(conditions) -> bool:
-	print(conditions)
+func apply_outcome(outcome: Event.State) -> void:
+	for item in outcome.items:
+		pass
+	
+	for flag in outcome.global:
+		_flags[flag] = outcome.global[flag]
+	
+	for stat in outcome.stats:
+		pass
+
+
+func are_conditions_met(conditions: Event.State) -> bool:
+	for item in conditions.items:
+		pass
+	
+	if not _are_flags_valid(conditions.global):
+		return false
+	
+	for stat in conditions.stats:
+		pass
+		
+	return true
+
+
+func _are_flags_valid(flags: Dictionary) -> bool:
+	for flag in flags:
+		var desired: bool = flags[flag]
+		var existing: bool = _flags.get(flag, false)
+		if existing != desired:
+			return false
 	return true
