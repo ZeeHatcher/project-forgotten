e6812e13b6f0a66c9c4102c4af08a1c66823ad86
Zircon <zircon332@gmail.com>
2023-05-18 22:15:14 +0800

    redesign ui

--- ui/journal/journal.gd
+++ ui/journal/journal.gd
@@ -3,7 +3,15 @@ extends TextureRect
 
 signal load_game(index)
 
+
+export(String) var unselected_text := "Choose an entry from the index."
+export(String) var uneventful_text := "Nothing important happened."
+
+var selected := 0
+
 onready var _item_list := $LeftPage/ItemList
+onready var _load_button := $Control/LoadButton
+onready var _content_text := $Control/RightPage/ContentText
 
 
 func _ready():
@@ -13,6 +21,9 @@ func _ready():
 func show() -> void:
 	visible = !visible
 	get_tree().paused = visible
+	_load_button.visible = false
+	_content_text.text = unselected_text
+	_content_text.bbcode_text = unselected_text
 
 
 func _load_save(index: int) -> void:
@@ -22,12 +33,11 @@ func _load_save(index: int) -> void:
 
 func _on_ItemList_item_selected(index):
 	print_debug("select " + str(index))
-
-
-func _on_ItemList_item_activated(index):
-	print_debug("loading " + str(index))
-	_load_save(index)
-	emit_signal("load_game", index)
+	selected = index
+	_load_button.visible = true
+	
+	_content_text.text = uneventful_text
+	_content_text.bbcode_text = uneventful_text
 
 
 func _on_GameSaveService_update_saves(save_count):
@@ -38,3 +48,9 @@ func _on_GameSaveService_update_saves(save_count):
 
 func _on_GameSaveService_show_journal():
 	show()
+
+
+func _on_LoadSaveLabel_pressed():
+	print_debug("loading " + str(selected))
+	_load_save(selected)
+	emit_signal("load_game", selected)
