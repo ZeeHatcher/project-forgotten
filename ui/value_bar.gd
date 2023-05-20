439a9579934f861291a9e900921bd240ca22e58b
ZeeHatcher <zee.hatcher010@gmail.com>
2023-05-20 21:31:04 +0800

    feat: animate bars

--- ui/value_bar.gd
+++ ui/value_bar.gd
@@ -2,8 +2,10 @@ extends TextureProgress
 
 
 export(Resource) var resource
+export var show_counter := true
 
 onready var _counter := $"%Counter"
+onready var _tween := $Tween
 
 
 func _ready() -> void:
@@ -13,8 +15,10 @@ func _ready() -> void:
 	
 	_counter.max_value = resource.max_value
 	_counter.value = resource.value
-
+	
+	_counter.visible = show_counter
 
 func _on_value_changed() -> void:
-	value = resource.value
+	_tween.interpolate_property(self, "value", value, resource.value, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	_tween.start()
 	_counter.value = resource.value
