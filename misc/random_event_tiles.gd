7daeb173416bb248a31cf59b6189f3dcb0b8ef1f
Zircon <zircon332@gmail.com>
2023-05-21 13:06:05 +0800

    added tiles sfx

--- misc/random_event_tiles.gd
+++ misc/random_event_tiles.gd
@@ -3,12 +3,64 @@ extends TileMap
 
 
 export(int, 0, 100) var trigger_chance
+export(NodePath) var _audio_player_path
 export var tile_events := {}
 export var grace_period := 5
+
+
+var tile_sounds := {
+	"forest": [
+		preload("res://assets/sfx/UX_forest_player_movement_1.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_2.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_3.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_4.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_5.mp3"),
+	],
+	"trees": [
+		preload("res://assets/sfx/UX_forest_player_movement_1.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_2.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_3.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_4.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_5.mp3"),
+	],
+	"grass": [
+		preload("res://assets/sfx/UX_forest_player_movement_1.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_2.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_3.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_4.mp3"),
+		preload("res://assets/sfx/UX_forest_player_movement_5.mp3"),
+	],
+	"mountain": [
+		preload("res://assets/sfx/UX_mountain_player_movement_1.mp3"),
+		preload("res://assets/sfx/UX_mountain_player_movement_2.mp3"),
+		preload("res://assets/sfx/UX_mountain_player_movement_3.mp3"),
+		preload("res://assets/sfx/UX_mountain_player_movement_4.mp3"),
+		preload("res://assets/sfx/UX_mountain_player_movement_5.mp3"),
+	],
+	"snow": [
+		preload("res://assets/sfx/UX_snow_player_movement_1.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_2.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_3.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_4.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_5.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_6.mp3"),
+	],
+	"snow_trees": [
+		preload("res://assets/sfx/UX_snow_player_movement_1.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_2.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_3.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_4.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_5.mp3"),
+		preload("res://assets/sfx/UX_snow_player_movement_6.mp3"),
+	]
+}
+
 
 signal event_triggered(event_name)
 
 var _grace := grace_period
+
+onready var _audio_player: AudioStreamPlayer = get_node(_audio_player_path)
 
 
 func trigger_event_at(global_pos: Vector2) -> void:
@@ -51,3 +103,15 @@ func _on_EventService_outcome_applied(outcome: Event.State):
 	print(mountain_id)
 	for i in range(tile_set.tile_get_shape_count(mountain_id)):
 		tile_set.tile_set_shape(mountain_id, i, null)
+
+
+func _on_Player_moved(new_pos):
+	var local_position := to_local(new_pos)
+	var map_position := world_to_map(local_position)
+	var tile_id := get_cellv(map_position)
+	var tile_type := tile_set.tile_get_name(tile_id)
+	
+	if tile_sounds.has(tile_type):
+		var rand_index:int = randi() % tile_sounds[tile_type].size()
+		_audio_player.stream = tile_sounds[tile_type][rand_index]
+		_audio_player.play()
