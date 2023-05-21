a85cd39bf7db972f07441cfbfe62bbd0f0a5ff50
ZeeHatcher <zee.hatcher010@gmail.com>
2023-05-21 16:56:03 +0800

    feat: redesign bar

--- ui/value_bar.gd
+++ ui/value_bar.gd
@@ -1,16 +1,17 @@
-extends TextureProgress
+extends Control
 
 
 export(Resource) var resource
 export var show_counter := true
 
 onready var _counter := $"%Counter"
+onready var _bar := $"%Bar"
 onready var _tween := $Tween
 
 
 func _ready() -> void:
-	max_value = resource.max_value
-	value = resource.value
+	_bar.max_value = resource.max_value
+	_bar.value = resource.value
 	resource.connect("value_changed", self, "_on_value_changed")
 	
 	_counter.max_value = resource.max_value
@@ -18,7 +19,8 @@ func _ready() -> void:
 	
 	_counter.visible = show_counter
 
+
 func _on_value_changed() -> void:
-	_tween.interpolate_property(self, "value", value, resource.value, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	_tween.interpolate_property(_bar, "value", _bar.value, resource.value, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	_tween.start()
 	_counter.value = resource.value
