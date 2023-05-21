1f91efec952ba0ea5aa30eafce9517c2df50aa2e
Zircon <zircon332@gmail.com>
2023-05-21 16:53:24 +0800

    added image and sfx for heli crash

--- scenes/heli_crash/heli_crash.gd
+++ scenes/heli_crash/heli_crash.gd
@@ -2,4 +2,8 @@ extends Control
 
 
 func _on_AudioStreamPlayer_finished():
+	$AnimationPlayer.play("show_crash")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene("res://scenes/tutorial/tutorial.tscn")
